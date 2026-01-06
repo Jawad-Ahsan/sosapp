0b897ddcec7e0baeaa4f62ffcc639e9027d3d9b9
Jawad-Ahsan <ahsanjawad70@gmail.com>
2026-01-06 13:40:40 +0500

    Fix models.py import

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Date, Boolean, Float
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-from database import Base
+from .database import Base
 
 class User(Base):
     __tablename__ = "users"
